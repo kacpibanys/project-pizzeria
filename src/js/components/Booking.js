0ce63d0867d3d839ae2bddbbb49811e81e3b5799
kacpibanys <kacpibanys@gmail.com>
2021-10-26 22:57:56 +0200

    fixes

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -215,10 +215,14 @@ class Booking {
     thisBooking.dom.wrapper.addEventListener('updated', function(){
       thisBooking.updateDOM();
     });
+    this.dom.wrapper.querySelector('.booking-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      thisBooking.sendBooking();
+    });
   }
   sendBooking() {
     const thisBooking = this;
-    const url = settings.db.url + '/' + settings.db.booking;
+    const url = settings.db.url + '/' + settings.db.bookings;
     const payload = {
       date: thisBooking.datePicker.value,
       hour: thisBooking.hourPicker.value,
@@ -226,15 +230,15 @@ class Booking {
       duration: parseInt(thisBooking.hoursAmount.value),
       ppl: parseInt(thisBooking.peopleAmount.value),
       starters: [],
-      phone: thisBooking.dom.phone.value,
-      address: thisBooking.dom.address.value,
+      phone: '44444',
+      address: '3434334@gmail.com',
     };
 
-    for (let starter of thisBooking.dom.starters) {
-      if (starter.checked == true) {
-        payload.starters.push(starter.value);
-      }
-    }
+    // for (let starter of thisBooking.dom.starters) {
+    //   if (starter.checked == true) {
+    //     payload.starters.push(starter.value);
+    //   }
+    // }
 
     const options = {
       method: 'POST',
